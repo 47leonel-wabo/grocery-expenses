2945e1f7038dc469e0b3ae0b54ca46563ec93a49
leonel47 <waboleonel@gmail.com>
2023-11-19 21:52:50 +0100

    Adding a loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import userService, { User } from "./services/user-service";
 import UserCard from "./components/UserCard";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState();
 
   useEffect(() => {
+    setLoading(true);
     const { controller, responsePromise } = userService.getAll<User>();
     responsePromise
       .then(({ data }) => setUsers(data))
-      .catch((error) => setError(error.message));
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
 
     return () => controller.abort();
   }, []);
@@ -22,6 +26,7 @@ function App() {
         Axios | JSONPlaceHolder <mark>Users</mark>
       </h4>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <LoadingSpinner />}
       <div className="row">
         {users.map((user) => (
           <UserCard key={user.id} userData={user} />
